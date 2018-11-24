2fc81b1301aaab3c92c193f7df97727a014be4f8
nargok <nargok@gmail.com>
2018-11-24 17:30:42 +0900

    InputObjectを使った実装にしたいがkeyword errorになるので一旦もとに戻す

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,16 +1,14 @@
 class Types::MutationType < Types::BaseObject
 
   # field :create_author, AuthorType, null: true, description: "Create an author" do
-  #   argument :first_name, String, required: false, camelize: false
-  #   argument :last_name, String, required: false, camelize: false
-  #   argument :yob, Int, required: false
-  #   argument :is_alive, Boolean, required: false, camelize: false
+  #   # argument :author, Types::AuthorInputType, required: true
   # end
 
+  # TODO AuthorInputTypeを使った実装にするとここでエラーがおきる　原因未調査
   # def create_author(first_name:, last_name:, yob:, is_alive:)
   #   Author.create(first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive)
   # end
 
+  # TODO AuthorInputTypeを使った実装でエラーが出るので、一旦GraphQL::Schema::Mutationの実装に戻す
   field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
-
 end
